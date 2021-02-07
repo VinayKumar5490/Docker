57bca8a0905d8849db8d32fe49d57a924449d775
VinayKumar5490 <vini.vinay77@yahoo.com>
2021-02-07 16:26:13 +0530

    Hello World

---
+++ src/main/java/com/test/general/hello/App.java
@@ -0,0 +1,14 @@
+package com.test.general.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
